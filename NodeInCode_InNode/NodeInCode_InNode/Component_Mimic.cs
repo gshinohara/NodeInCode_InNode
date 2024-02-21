525ef254928658b1a78799930fa700420cc4de8e
gshinohara <65073029+gshinohara@users.noreply.github.com>
2024-02-22 01:32:38 +0900

    入力コンポーネントを再現する機能

--- NodeInCode_InNode/NodeInCode_InNode/Component_Mimic.cs
+++ NodeInCode_InNode/NodeInCode_InNode/Component_Mimic.cs
@@ -1,16 +1,12 @@
 ﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
 using System;
-using Rhino.NodeInCode;
 using System.Linq;
-using Grasshopper.Kernel.Parameters;
-using Grasshopper.Kernel.Data;
 
 namespace NodeInCode_InNode
 {
     public class Component_Mimic : GH_Component
     {
-        private Guid Parent_ComponentGuid => new Guid("a0d62394-a118-422d-abb3-6af115c75b25");//Addition
-        private ComponentFunctionInfo Component {  get; set; }
         public Component_Mimic()
           : base("Mimic", "Mimic",
               "",
@@ -19,26 +15,32 @@ namespace NodeInCode_InNode
         }
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddGenericParameter("Component", "C", "", GH_ParamAccess.item);
         }
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
         }
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            object a = null;
-            object b = null;
-            DA.GetData("a", ref a);
-            DA.GetData("b", ref b);
-            DA.SetDataList("result", (Component.InvokeKeepTree(a, b).FirstOrDefault() as Grasshopper.DataTree<object>).AllData());
         }
-        public override void AddedToDocument(GH_Document document)
+        protected override void BeforeSolveInstance()
         {
-            base.AddedToDocument(document);
-            string name = Components.NodeInCodeFunctions.GetDynamicMemberNames().FirstOrDefault(n => Components.FindComponent(n).ComponentGuid == Parent_ComponentGuid);
-            Component = Components.FindComponent(name);
-            this.Params.Input.AddRange(Component.InputNames.Select(n => new Param_GenericObject { Name = n, NickName = n }));
-            this.Params.Output.AddRange(Component.OutputNames.Select(n => new Param_GenericObject { Name = n, NickName = n }));
-            this.Message = Component.FullName;
+            if (this.Params.Input.FirstOrDefault().VolatileDataCount == 0)
+                return;
+
+            IGH_Component component = (this.Params.Input.FirstOrDefault(p => p.Name == "Component").VolatileData.AllData(true).FirstOrDefault() as GH_ObjectWrapper).Value as IGH_Component;
+            Func<IGH_Param, IGH_Param> get_instance = param =>
+            {
+                var a = param.GetType().GetConstructor(Type.EmptyTypes).Invoke(null) as IGH_Param;
+                a.Name = param.Name;
+                a.NickName = param.NickName;
+                a.Access = param.Access;
+                return a;
+            };
+            foreach (IGH_Param param in component.Params.Input)
+                this.Params.RegisterInputParam(get_instance(param));
+            foreach (IGH_Param param in component.Params.Output)
+                this.Params.RegisterOutputParam(get_instance(param));
         }
         protected override System.Drawing.Bitmap Icon => null;
         public override Guid ComponentGuid => new Guid("A44C410E-0D57-456E-B45A-9E057FA6DABE");
